f9291e142f7aa925b24f7bf0845a524451949368
oooblack <1546080420@qq.com>
2019-09-02 18:43:37 +0800

    之前的一些笔记demo

--- xiayu/src/main/java/com/xiayu/JavaDemo/xiayu/java/java/io/nio/netty/DiscardServer.java
+++ xiayu/src/main/java/com/xiayu/JavaDemo/xiayu/java/java/io/nio/netty/DiscardServer.java
@@ -1,6 +1,13 @@
 package com.xiayu.JavaDemo.xiayu.java.java.io.nio.netty;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 
 /**
  * @author: test_
@@ -14,7 +21,29 @@ public class DiscardServer {
     }
 
     public void run() {
+        //处理I/O操作的多线程事件循环
         NioEventLoopGroup bossGroup = new NioEventLoopGroup();
         NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class).childHandler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel sc) throws Exception {
+                    sc.pipeline().addLast(new DiscardServerHandler());
+                }
+            }).option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+            ChannelFuture future = serverBootstrap.bind(port).sync();
+            future.channel().closeFuture().sync();
+        } catch (Exception ignored) {
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        new DiscardServer(8080).run();
     }
 }
---
+++ xiayu/src/main/java/com/xiayu/JavaDemo/xiayu/online/common/config/ShiroConfig.java
@@ -0,0 +1,26 @@
+package com.xiayu.JavaDemo.xiayu.online.common.config;
+
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * @author: test_
+ * @date: 2019/9/2
+ */
+@Configuration
+public class ShiroConfig {
+    /**
+     * ShiroFilterFactoryBean 处理拦截资源文件问题。
+     * 注意：单独一个ShiroFilterFactoryBean配置是或报错的，以为在
+     * 初始化ShiroFilterFactoryBean的时候需要注入：SecurityManager
+     *
+     * Filter Chain定义说明 1、一个URL可以配置多个Filter，使用逗号分隔 2、当设置多个过滤器时，全部验证通过，才视为通过
+     * 3、部分过滤器可指定参数，如perms，roles
+     *
+     */
+}
