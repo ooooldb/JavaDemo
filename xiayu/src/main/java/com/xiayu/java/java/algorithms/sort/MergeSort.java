ec10ed01ae604bd96a513f303a3fdac3274258ea
oooblack <oooldblack@gmail.com>
2019-10-21 22:45:30 +0800

    sort

--- xiayu/src/main/java/com/xiayu/java/java/algorithms/sort/MergeSort.java
+++ xiayu/src/main/java/com/xiayu/java/java/algorithms/sort/MergeSort.java
@@ -1,10 +1,9 @@
 package com.xiayu.java.java.algorithms.sort;
 
 /**
- * @author: test_
+ * @author: starc
  * @date: 2019/10/21
  */
-public class HeapSort {
-    private int[] heap;
+public class MergeSort {
 
 }
