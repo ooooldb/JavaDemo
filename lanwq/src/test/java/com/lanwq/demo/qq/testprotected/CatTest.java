4a36480145ffa19929f67b7dd5cd503321ac658d
QuanWenLan <670453221@qq.com>
2019-12-25 22:08:17 +0800

    提交文件夹

---
+++ lanwq/src/test/java/com/lanwq/demo/qq/testprotected/CatTest.java
@@ -0,0 +1,13 @@
+package com.lanwq.demo.qq.testprotected;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CatTest {
+
+    @Test
+    public void show() {
+        System.out.println("你大爷的，让我弄这么久。。。");
+    }
+}
